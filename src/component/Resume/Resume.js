2a1ed81d58fadb08b013e70679699c11b8837dc2
Paul Keno <paulkenoofficial.com>
2021-10-07 11:29:55 +0100

    resume tools and online courses

--- src/component/Resume/Resume.js
+++ src/component/Resume/Resume.js
@@ -30,19 +30,70 @@ const Resume = () => {
 
                 <div className={styles["div-section"]}>
                     <h2> Education </h2>
+                    <div className={styles["sub-div"]} >
+                        <h3> University of Benin</h3>
+                        <br />
+                        <p>  B.ENG Electrical/Electronics Engineering </p>
+                        <p> 2012 -2017 </p>
+                    </div>
+                   
+
+
+
+
 
                 </div>
 
             </section>
 
+
             <section className={styles["resume-section"]}>
 
                 <div className={styles["div-section"]}>
                     <h2> Skills </h2>
+
+                    <div className={styles["skills-section"]}> 
+
+                        <div className={styles["sub-div"]} >
+                            <h3> Development</h3>
+                            <br />
+                            <p> HTML5 </p>
+                            <p> CSS3 </p>
+                            <p> JavaScript </p>
+                            <p> React </p>
+                        </div>
+
+                        
+
+                        <div className={styles["sub-div"]} >
+                            <h3> Tools </h3>
+                            <br />
+                            
+                            <p> Firebase </p>
+                            <p> GitHub </p>
+                            <p> Postman </p>
+                            <p> Visual Studio Code </p>
+                        
+                        </div>
+
+                    </div>
                 </div>
 
                 <div className={styles["div-section"]}>
                      <h2> Online Courses </h2>
+                     <div className={styles["sub-div"]} >
+                        <h3> Codecademy</h3>
+                        <br />
+                        <p>  Certified JavaScript Web Developer </p>
+                        <p> 2021 </p>
+                    </div>
+
+                     <div className={styles["sub-div"]} >
+                        <h3> Udemy </h3>
+                        <br />
+                        <p>  The Complete Web Development Bootcamp </p>
+                        <p> 2021 </p>
+                    </div>
                 </div>
 
             </section>
